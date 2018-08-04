17032fd4f9062f6e4b2001ed01bb82fb102db89a
Oliver Brakmann <ob-web2013+github@posteo.net>
2018-08-04 23:34:22 +0200

    Fix copyright notices

--- OpenRA.Mods.Fuel/Traits/RefuelsUnits.cs
+++ OpenRA.Mods.Fuel/Traits/RefuelsUnits.cs
@@ -1,10 +1,10 @@
 #region Copyright & License Information
 /*
- * Copyright 2007-2015 The OpenRA Developers (see AUTHORS)
- * This file is part of OpenRA, which is free software. It is made
- * available to you under the terms of the GNU General Public License
- * as published by the Free Software Foundation. For more information,
- * see COPYING.
+ * Copyright 2015-2018 Oliver Brakmann
+ * This file is part of the OpenRA Fuel Plugin, which is free software.
+ * It is made available to you under the terms of the GNU General Public
+ * License as published by the Free Software Foundation. For more
+ * information, see COPYING.
  */
 #endregion
 
